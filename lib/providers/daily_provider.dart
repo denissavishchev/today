4327c8540450a995fd67565adeff48c10dc39e71
Denis Savishchev <denis.savishchev@gmail.com>
2023-10-05 20:34:13 +0200

    add daily page

--- lib/providers/daily_provider.dart
+++ lib/providers/daily_provider.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 class DailyProvider with ChangeNotifier {
 
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
 
 }
