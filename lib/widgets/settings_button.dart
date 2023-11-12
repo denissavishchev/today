e151bc43f7cefa11a6f149e131e5acc9829b6794
Denis Savishchev <denis.savishchev@gmail.com>
2023-11-12 14:43:19 +0100

    water settings page done

---
+++ lib/widgets/settings_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
+import 'package:today/widgets/side_button_widget.dart';
+import '../constants.dart';
+import '../model/boxes.dart';
+import '../model/water_model.dart';
+import '../pages/water_settings_page.dart';
+import '../providers/water_provider.dart';
+
+class SettingsButton extends StatelessWidget {
+  const SettingsButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<WaterProvider>(
+        builder: (context, data, _){
+          return ValueListenableBuilder(
+              valueListenable: Boxes.addWaterSettingsToBase().listenable(),
+              builder: (context, box, _){
+                final settings = box.values.toList().cast<WaterSettingsModel>();
+                return SideButtonWidget(
+                  width: 100,
+                  both: true,
+                  onTap: () {
+                    data.target = settings.isEmpty ? 0 : settings[0].target;
+                    data.weight = settings.isEmpty ? '000' : settings[0].weight;
+                    data.initialWakeUpTime = settings.isEmpty
+                        ? const TimeOfDay(hour: 8, minute: 00)
+                        : TimeOfDay(hour:int.parse((settings[0].wakeUpTime).split(":")[0]),
+                        minute: int.parse((settings[0].wakeUpTime).split(":")[1]));
+                    data.initialBedTime = settings.isEmpty
+                        ? const TimeOfDay(hour: 22, minute: 00)
+                        : TimeOfDay(hour:int.parse((settings[0].bedTime).split(":")[0]),
+                        minute: int.parse((settings[0].bedTime).split(":")[1]));
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) =>
+                        const WaterSettingsPage()));
+                  },
+
+                  child: Icon(Icons.settings,
+                    color: kOrange.withOpacity(0.7),
+                    size: 40,),);
+              });
+        });
+  }
+}
