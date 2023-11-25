fb297c59c8d12c48cda422c546443cbf9e397a12
Denis Savishchev <denis.savishchev@gmail.com>
2023-11-25 14:30:57 +0100

    interval test

--- lib/providers/hive_provider.dart
+++ lib/providers/hive_provider.dart
@@ -7,47 +7,40 @@ class HiveProvider with ChangeNotifier{
 
   late Timer timer;
 
+  // int daysBetween(DateTime from, DateTime to) {
+  //   from = DateTime(from.year, from.month, from.day);
+  //   to = DateTime(to.year, to.month, to.day);
+  //   return (to.difference(from).inHours / 24).round();
+  // }
+
   Future<void> addNotificationData(TimeOfDay start, TimeOfDay end, int interval) async {
     Box box = Hive.box('notifications');
     box.put('start', start.toString());
     box.put('end', end.toString());
     box.put('interval', interval);
     notifyListeners();
-    print(box.get('start').toString().substring(10, 12));
-    print(box.get('start').toString().substring(13, 15));
-    print(box.get('interval'));
   }
 
   Future sendNotification() async{
     Box box = Hive.box('notifications');
-    for(var i = 0; i < 5; i++){
+    int start = int.parse(box.get('start').toString().substring(10, 12));
+    int end = int.parse(box.get('end').toString().substring(10, 12));
+    int interval = box.get('interval');
+    for(var i = start; i < end; i+interval){
       await AwesomeNotifications().createNotification(
         content: NotificationContent(
           id: DateTime.now().microsecondsSinceEpoch.remainder(200),
           channelKey: 'scheduled',
-          title: '${Emojis.symbols_potable_water} Drink some water',
+          title: '${Emojis.animals_camel} Drink some water',
           body: 'startTime: ${DateTime.now().hour} Time: ${DateTime.now().hour + i}',
         ),
         schedule: NotificationCalendar(
-            hour: int.parse(box.get('start').toString().substring(10, 12)) + i,
+            hour: i,
             minute: int.parse(box.get('start').toString().substring(13, 15)),
             second: 0,
             repeats: true
         ),
       );
     }
-    // timer = Timer.periodic(const Duration(hours: 1), (timer) async {
-    //   await AwesomeNotifications().createNotification(
-    //     content: NotificationContent(
-    //       id: DateTime.now().microsecondsSinceEpoch.remainder(200),
-    //       channelKey: 'scheduled',
-    //       title: '${Emojis.symbols_potable_water} Drink some water',
-    //       body: 'End: ${DateTime.now().hour}',
-    //     ),
-    //   );
-    // });
-    // if(TimeOfDay.now() == end){
-    //   timer.cancel();
-    // }
   }
 }
--- lib/providers/water_provider.dart
+++ lib/providers/water_provider.dart
@@ -266,11 +266,4 @@ class WaterProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  // int daysBetween(DateTime from, DateTime to) {
-  //   from = DateTime(from.year, from.month, from.day);
-  //   to = DateTime(to.year, to.month, to.day);
-  //   return (to.difference(from).inHours / 24).round();
-  // }
-
-
 }
